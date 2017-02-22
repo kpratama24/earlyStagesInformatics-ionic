50805f52ad070ff2b275bd9f014c6c635b596ca3
Carissa Tobing <Carissa Tobing>
2017-02-22 13:16:02 +0700

    Template dasar, bikin semua pages dasar. belum diedit kontentnya.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { News } from '../pages/News/news';
+import { Fixtures } from '../pages/Fixtures/fixtures';
+import { Rugby101 } from '../pages/Rugby101/rugby101';
+import { RugbyClubs } from '../pages/RugbyClubs/rugbyclubs';
+import { TeamPhotos } from '../pages/TeammatePhotos/teamphotos';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+  News, Fixtures, Rugby101, RugbyClubs, TeamPhotos
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +18,8 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+
+      News, Fixtures, Rugby101, RugbyClubs, TeamPhotos
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ src/pages/TeammatePhotos/teamphotos.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-teamphotos',
+  templateUrl: 'teamphotos.html'
+})
+export class TeamPhotos {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
