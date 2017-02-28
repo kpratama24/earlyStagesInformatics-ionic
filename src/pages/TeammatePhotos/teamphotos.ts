0487d1985dba900a85929391b59afe1396216ecc
Kevin Pratama <Kevin Pratama>
2017-02-28 15:52:41 +0700

    Add taking picture feature to TeamPhotos pages

--- src/pages/TeammatePhotos/teamphotos.ts
+++ src/pages/TeammatePhotos/teamphotos.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {Camera} from 'ionic-native';
+
 @Component({
   selector: 'page-teamphotos',
   templateUrl: 'teamphotos.html'
@@ -10,6 +12,15 @@ export class TeamPhotos {
 
   constructor(public navCtrl: NavController) {
 
-  }
+    }
 
+    public takePhotos(){
+      Camera.getPicture().then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+      // Handle error
+      });
+    }
 }
