869494f7a8dae1e91f58c58caf671ed0fb494054
Nadiar Halida <Nadiar Halida>
2017-03-05 14:49:04 +0700

    Karena belum jalan jadi dibuat komen dulu isi news.ts nya

--- src/pages/News/news.ts
+++ src/pages/News/news.ts
@@ -14,9 +14,12 @@ export class News {
   map untuk konversi data dari bentuk String ke bentuk json yang bisa dibaca
   Subscribe untuk akses datanya dan disimpan ke var newsInfo
   */
-  constructor(public navCtrl: NavController, public http: Http) {
-    this.http.get('https://ri-admin.azurewebsites.net/indonesianrugby/news/list.json').map(res => res.json()).subscribe(data => {
-        this.newsInfo = data.data.children;
-    });
-  }
+
+
+  // constructor(public navCtrl: NavController, public http: Http) {
+  //   this.http= http;
+  //   this.http.get('https://ri-admin.azurewebsites.net/indonesianrugby/news/list.json').map(res => res.json()).subscribe(data => {
+  //       this.newsInfo = data.data.children;
+  //   });
+  // }
 }
