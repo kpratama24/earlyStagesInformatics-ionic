7096929c45a98278803c77ed8c6caab399e65040
Reza Haznam <rezareynaldihh@gmail.com>
2017-03-29 22:15:39 +0700

    Json news udah bs, klo blm rapi tolong dbnerin di htmlnya, rugbyService.ts ignore aja, ga kepake

--- src/pages/News/news.ts
+++ src/pages/News/news.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import {RugbyService} from '../../app/services/rugbyService'
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -9,17 +10,14 @@ import 'rxjs/add/operator/map';
 })
 export class News {
   public newsInfo: any;
+
   /*
   http.get untuk ambil data dari web
-  map untuk konversi data dari bentuk String ke bentuk json yang bisa dibaca
   Subscribe untuk akses datanya dan disimpan ke var newsInfo
   */
 
-
-  // constructor(public navCtrl: NavController, public http: Http) {
-  //   this.http= http;
-  //   this.http.get('https://ri-admin.azurewebsites.net/indonesianrugby/news/list.json').map(res => res.json()).subscribe(data => {
-  //       this.newsInfo = data.data.children;
-  //   });
-  // }
+  constructor(private http:Http) {
+    this.http.get('https://ri-admin.azurewebsites.net/indonesianrugby/news/list.json')
+            .subscribe(data => this.newsInfo = data.json());
+  }
 }
